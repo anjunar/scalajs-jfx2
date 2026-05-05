0523993dcd6c142e458713e688ddb8db16d02180
Patrick Bittner <anjunar@gmx.de>
2026-05-05 14:30:24 +0200

    edit: add factory property

--- jfx-forms/src/main/scala/jfx/form/SubForm.scala
+++ jfx-forms/src/main/scala/jfx/form/SubForm.scala
@@ -10,10 +10,24 @@ class SubForm[M](val $name: String) extends Box("fieldset") with Formular[M] wit
   override val tagName: String = "fieldset"
 
   override val $valueProperty: Property[M] = Property(null.asInstanceOf[M])
+  private var _factory: (() => M) | Null = null
+
+  def $factory: (() => M) | Null = _factory
+  def $factory_=(value: () => M): Unit =
+    _factory = value
+
+  override def registerControl(control: Control[?]): Unit = {
+    super.registerControl(control)
+    control.$editableProperty.set($editableProperty.get)
+  }
 
   override def compose(): Unit = {
     given jfx.core.component.Component = this
     super.compose()
+
+    addDisposable($editableProperty.observe { editable =>
+      $controls.foreach(_. $editableProperty.set(editable))
+    })
 
     if (!$standalone) {
       try {
@@ -23,6 +37,20 @@ class SubForm[M](val $name: String) extends Box("fieldset") with Formular[M] wit
       } catch {
         case _: Exception => 
       }
+    }
+  }
+
+  def clearForm(): Unit = {
+    $valueProperty.set(null.asInstanceOf[M])
+    clearErrors()
+    resetInteractionState()
+  }
+
+  def newInstance(): Unit = {
+    Option(_factory).foreach { factory =>
+      $valueProperty.set(factory())
+      clearErrors()
+      resetInteractionState()
     }
   }
 }
@@ -40,4 +68,8 @@ object SubForm {
   def editable[M](using f: SubForm[M]): Boolean = f.$editable
   def editable_=[M](using f: SubForm[M])(value: Boolean): Unit = f.$editable = value
   def editableProperty[M](using f: SubForm[M]): Property[Boolean] = f.$editableProperty
+  def factory[M](using f: SubForm[M]): (() => M) | Null = f.$factory
+  def factory_=[M](using f: SubForm[M])(value: () => M): Unit = f.$factory = value
+  def clearForm[M]()(using f: SubForm[M]): Unit = f.clearForm()
+  def newInstance[M]()(using f: SubForm[M]): Unit = f.newInstance()
 }
