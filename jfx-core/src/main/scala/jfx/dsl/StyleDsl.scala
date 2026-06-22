e874220a44946c51f6f9e3bf4a12d2e5006c030c
Patrick Bittner <anjunar@gmx.de>
2026-06-22 11:15:31 +0200

    edit: drawer with mode overlay or not

--- jfx-core/src/main/scala/jfx/dsl/StyleDsl.scala
+++ jfx-core/src/main/scala/jfx/dsl/StyleDsl.scala
@@ -12,171 +12,201 @@ object StyleDsl {
     init(using proxy)
   }
 
+  private def set(name: String, value: String)(using s: StyleProxy): Unit =
+    s.host.setStyle(name, value)
+
+  private def bind(name: String, value: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit =
+    c.addDisposable(value.observe(v => s.host.setStyle(name, v)))
+
   def boxSizing(using s: StyleProxy): String = ""
-  def boxSizing_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("box-sizing", v)
+  def boxSizing_=(v: String)(using s: StyleProxy): Unit = set("box-sizing", v)
+  def boxSizing_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("box-sizing", v)
 
   def width(using s: StyleProxy): String = ""
-  def width_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("width", v)
-  def width_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit =
-    c.addDisposable(v.observe(s.host.setStyle("width", _)))
+  def width_=(v: String)(using s: StyleProxy): Unit = set("width", v)
+  def width_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("width", v)
 
   def height(using s: StyleProxy): String = ""
-  def height_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("height", v)
-  def height_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit =
-    c.addDisposable(v.observe(s.host.setStyle("height", _)))
+  def height_=(v: String)(using s: StyleProxy): Unit = set("height", v)
+  def height_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("height", v)
 
   def transform(using s: StyleProxy): String = ""
-  def transform_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("transform", v)
-  def transform_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit =
-    c.addDisposable(v.observe(s.host.setStyle("transform", _)))
+  def transform_=(v: String)(using s: StyleProxy): Unit = set("transform", v)
+  def transform_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("transform", v)
 
   def top(using s: StyleProxy): String = ""
-  def top_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("top", v)
-  def top_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit =
-    c.addDisposable(v.observe(s.host.setStyle("top", _)))
-
+  def top_=(v: String)(using s: StyleProxy): Unit = set("top", v)
+  def top_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("top", v)
 
   def minHeight(using s: StyleProxy): String = ""
-  def minHeight_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("min-height", v)
-  def minHeight_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit =
-    c.addDisposable(v.observe(s.host.setStyle("min-height", _)))
+  def minHeight_=(v: String)(using s: StyleProxy): Unit = set("min-height", v)
+  def minHeight_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("min-height", v)
 
   def minWidth(using s: StyleProxy): String = ""
-  def minWidth_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("min-width", v)
-  def minWidth_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit =
-    c.addDisposable(v.observe(s.host.setStyle("min-width", _)))
+  def minWidth_=(v: String)(using s: StyleProxy): Unit = set("min-width", v)
+  def minWidth_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("min-width", v)
 
   def maxHeight(using s: StyleProxy): String = ""
-  def maxHeight_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("max-height", v)
-  def maxHeight_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit =
-    c.addDisposable(v.observe(s.host.setStyle("max-height", _)))
+  def maxHeight_=(v: String)(using s: StyleProxy): Unit = set("max-height", v)
+  def maxHeight_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("max-height", v)
 
   def maxWidth(using s: StyleProxy): String = ""
-  def maxWidth_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("max-width", v)
-  def maxWidth_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit =
-    c.addDisposable(v.observe(s.host.setStyle("max-width", _)))
+  def maxWidth_=(v: String)(using s: StyleProxy): Unit = set("max-width", v)
+  def maxWidth_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("max-width", v)
 
   def marginTop(using s: StyleProxy): String = ""
-  def marginTop_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("margin-top", v)
-  def marginTop_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit =
-    c.addDisposable(v.observe(s.host.setStyle("margin-top", _)))
+  def marginTop_=(v: String)(using s: StyleProxy): Unit = set("margin-top", v)
+  def marginTop_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("margin-top", v)
 
   def marginRight(using s: StyleProxy): String = ""
-  def marginRight_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("margin-right", v)
+  def marginRight_=(v: String)(using s: StyleProxy): Unit = set("margin-right", v)
+  def marginRight_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("margin-right", v)
 
   def marginBottom(using s: StyleProxy): String = ""
-  def marginBottom_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("margin-bottom", v)
-  def marginBottom_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit =
-    c.addDisposable(v.observe(s.host.setStyle("margin-bottom", _)))
+  def marginBottom_=(v: String)(using s: StyleProxy): Unit = set("margin-bottom", v)
+  def marginBottom_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("margin-bottom", v)
 
   def marginLeft(using s: StyleProxy): String = ""
-  def marginLeft_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("margin-left", v)
+  def marginLeft_=(v: String)(using s: StyleProxy): Unit = set("margin-left", v)
+  def marginLeft_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("margin-left", v)
 
   def display(using s: StyleProxy): String = ""
-  def display_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("display", v)
-  def display_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit =
-    c.addDisposable(v.observe(s.host.setStyle("display", _)))
+  def display_=(v: String)(using s: StyleProxy): Unit = set("display", v)
+  def display_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("display", v)
 
   def flexDirection(using s: StyleProxy): String = ""
-  def flexDirection_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("flex-direction", v)
+  def flexDirection_=(v: String)(using s: StyleProxy): Unit = set("flex-direction", v)
+  def flexDirection_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("flex-direction", v)
 
   def alignItems(using s: StyleProxy): String = ""
-  def alignItems_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("align-items", v)
+  def alignItems_=(v: String)(using s: StyleProxy): Unit = set("align-items", v)
+  def alignItems_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("align-items", v)
 
   def justifyContent(using s: StyleProxy): String = ""
-  def justifyContent_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("justify-content", v)
+  def justifyContent_=(v: String)(using s: StyleProxy): Unit = set("justify-content", v)
+  def justifyContent_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("justify-content", v)
 
   def flex(using s: StyleProxy): String = ""
-  def flex_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("flex", v)
+  def flex_=(v: String)(using s: StyleProxy): Unit = set("flex", v)
+  def flex_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("flex", v)
 
   def gap(using s: StyleProxy): String = ""
-  def gap_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("gap", v)
+  def gap_=(v: String)(using s: StyleProxy): Unit = set("gap", v)
+  def gap_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("gap", v)
 
   def padding(using s: StyleProxy): String = ""
-  def padding_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("padding", v)
-  def padding_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit =
-    c.addDisposable(v.observe(s.host.setStyle("padding", _)))
+  def padding_=(v: String)(using s: StyleProxy): Unit = set("padding", v)
+  def padding_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("padding", v)
 
   def background(using s: StyleProxy): String = ""
-  def background_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("background", v)
+  def background_=(v: String)(using s: StyleProxy): Unit = set("background", v)
+  def background_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("background", v)
 
   def backgroundColor(using s: StyleProxy): String = ""
-  def backgroundColor_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("background-color", v)
+  def backgroundColor_=(v: String)(using s: StyleProxy): Unit = set("background-color", v)
+  def backgroundColor_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("background-color", v)
 
   def borderRadius(using s: StyleProxy): String = ""
-  def borderRadius_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("border-radius", v)
+  def borderRadius_=(v: String)(using s: StyleProxy): Unit = set("border-radius", v)
+  def borderRadius_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("border-radius", v)
 
   def border(using s: StyleProxy): String = ""
-  def border_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("border", v)
+  def border_=(v: String)(using s: StyleProxy): Unit = set("border", v)
+  def border_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("border", v)
 
   def borderBottom(using s: StyleProxy): String = ""
-  def borderBottom_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("border-bottom", v)
+  def borderBottom_=(v: String)(using s: StyleProxy): Unit = set("border-bottom", v)
+  def borderBottom_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("border-bottom", v)
 
   def borderTop(using s: StyleProxy): String = ""
-  def borderTop_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("border-top", v)
+  def borderTop_=(v: String)(using s: StyleProxy): Unit = set("border-top", v)
+  def borderTop_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("border-top", v)
 
   def borderLeft(using s: StyleProxy): String = ""
-  def borderLeft_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("border-left", v)
+  def borderLeft_=(v: String)(using s: StyleProxy): Unit = set("border-left", v)
+  def borderLeft_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("border-left", v)
 
   def borderRight(using s: StyleProxy): String = ""
-  def borderRight_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("border-right", v)
+  def borderRight_=(v: String)(using s: StyleProxy): Unit = set("border-right", v)
+  def borderRight_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("border-right", v)
 
   def fontWeight(using s: StyleProxy): String = ""
-  def fontWeight_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("font-weight", v)
+  def fontWeight_=(v: String)(using s: StyleProxy): Unit = set("font-weight", v)
+  def fontWeight_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("font-weight", v)
 
   def textAlign(using s: StyleProxy): String = ""
-  def textAlign_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("text-align", v)
+  def textAlign_=(v: String)(using s: StyleProxy): Unit = set("text-align", v)
+  def textAlign_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("text-align", v)
 
   def color(using s: StyleProxy): String = ""
-  def color_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("color", v)
+  def color_=(v: String)(using s: StyleProxy): Unit = set("color", v)
+  def color_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("color", v)
 
   def fontSize(using s: StyleProxy): String = ""
-  def fontSize_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("font-size", v)
+  def fontSize_=(v: String)(using s: StyleProxy): Unit = set("font-size", v)
+  def fontSize_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("font-size", v)
 
   def opacity(using s: StyleProxy): String = ""
-  def opacity_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("opacity", v)
+  def opacity_=(v: String)(using s: StyleProxy): Unit = set("opacity", v)
+  def opacity_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("opacity", v)
 
   def position(using s: StyleProxy): String = ""
-  def position_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("position", v)
+  def position_=(v: String)(using s: StyleProxy): Unit = set("position", v)
+  def position_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("position", v)
 
   def right(using s: StyleProxy): String = ""
-  def right_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("right", v)
+  def right_=(v: String)(using s: StyleProxy): Unit = set("right", v)
+  def right_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("right", v)
 
   def bottom(using s: StyleProxy): String = ""
-  def bottom_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("bottom", v)
+  def bottom_=(v: String)(using s: StyleProxy): Unit = set("bottom", v)
+  def bottom_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("bottom", v)
 
   def left(using s: StyleProxy): String = ""
-  def left_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("left", v)
+  def left_=(v: String)(using s: StyleProxy): Unit = set("left", v)
+  def left_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("left", v)
 
   def zIndex(using s: StyleProxy): String = ""
-  def zIndex_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("z-index", v)
-  def zIndex_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit =
-    c.addDisposable(v.observe(s.host.setStyle("z-index", _)))
+  def zIndex_=(v: String)(using s: StyleProxy): Unit = set("z-index", v)
+  def zIndex_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("z-index", v)
 
   def overflow(using s: StyleProxy): String = ""
-  def overflow_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("overflow", v)
+  def overflow_=(v: String)(using s: StyleProxy): Unit = set("overflow", v)
+  def overflow_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("overflow", v)
 
   def overflowY(using s: StyleProxy): String = ""
-  def overflowY_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("overflow-y", v)
+  def overflowY_=(v: String)(using s: StyleProxy): Unit = set("overflow-y", v)
+  def overflowY_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("overflow-y", v)
 
   def overflowX(using s: StyleProxy): String = ""
-  def overflowX_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("overflow-x", v)
+  def overflowX_=(v: String)(using s: StyleProxy): Unit = set("overflow-x", v)
+  def overflowX_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("overflow-x", v)
 
   def boxShadow(using s: StyleProxy): String = ""
-  def boxShadow_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("box-shadow", v)
+  def boxShadow_=(v: String)(using s: StyleProxy): Unit = set("box-shadow", v)
+  def boxShadow_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("box-shadow", v)
 
   def objectFit(using s: StyleProxy): String = ""
-  def objectFit_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("object-fit", v)
+  def objectFit_=(v: String)(using s: StyleProxy): Unit = set("object-fit", v)
+  def objectFit_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("object-fit", v)
 
   def objectPosition(using s: StyleProxy): String = ""
-  def objectPosition_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("object-position", v)
+  def objectPosition_=(v: String)(using s: StyleProxy): Unit = set("object-position", v)
+  def objectPosition_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("object-position", v)
 
   def cursor(using s: StyleProxy): String = ""
-  def cursor_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("cursor", v)
+  def cursor_=(v: String)(using s: StyleProxy): Unit = set("cursor", v)
+  def cursor_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("cursor", v)
 
   def pointerEvents(using s: StyleProxy): String = ""
-  def pointerEvents_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("pointer-events", v)
+  def pointerEvents_=(v: String)(using s: StyleProxy): Unit = set("pointer-events", v)
+  def pointerEvents_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("pointer-events", v)
 
   def flexWrap(using s: StyleProxy): String = ""
-  def flexWrap_=(v: String)(using s: StyleProxy): Unit = s.host.setStyle("flex-wrap", v)
+  def flexWrap_=(v: String)(using s: StyleProxy): Unit = set("flex-wrap", v)
+  def flexWrap_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("flex-wrap", v)
+
+  def transition(using s: StyleProxy): String = ""
+  def transition_=(v: String)(using s: StyleProxy): Unit = set("transition", v)
+  def transition_=(v: ReadOnlyProperty[String])(using s: StyleProxy, c: Component): Unit = bind("transition", v)
 }
--- jfx-core/src/main/scala/jfx/layout/Drawer.scala
+++ jfx-core/src/main/scala/jfx/layout/Drawer.scala
@@ -11,6 +11,7 @@ class Drawer extends Box("div") {
   val $openProperty = Property(false)
   val $drawerWidthProperty = Property("280px")
   val $sideProperty = Property(Drawer.Side.Start)
+  val $modeProperty = Property(Drawer.Mode.Push)
   val $closeOnScrimClickProperty = Property(true)
 
   private var _navigationHost: Box = uninitialized
@@ -19,47 +20,161 @@ class Drawer extends Box("div") {
   def $navigationHost: Box = _navigationHost
   def $contentHost: Box = _contentHost
 
+  private val resolvedDrawerWidthProperty =
+    $drawerWidthProperty.map { width =>
+      s"min(92vw, $width)"
+    }
+
+  private val drawerFlexDirectionProperty =
+    $sideProperty.map {
+      case Drawer.Side.Start => "row"
+      case Drawer.Side.End   => "row-reverse"
+    }
+
   private val panelShellWidthProperty =
-    $openProperty.flatMap { open =>
-      $drawerWidthProperty.map { width =>
-        if (open) s"min(92vw, $width)" else "0px"
-      }
+    $modeProperty.flatMap {
+      case Drawer.Mode.Push =>
+        $openProperty.flatMap { open =>
+          resolvedDrawerWidthProperty.map { width =>
+            if (open) width else "0px"
+          }
+        }
+
+      case Drawer.Mode.Overlay =>
+        resolvedDrawerWidthProperty
+    }
+
+  private val panelShellPositionProperty =
+    $modeProperty.map {
+      case Drawer.Mode.Push    => "relative"
+      case Drawer.Mode.Overlay => "absolute"
+    }
+
+  private val panelShellLeftProperty =
+    $modeProperty.flatMap {
+      case Drawer.Mode.Push =>
+        $sideProperty.map(_ => "auto")
+
+      case Drawer.Mode.Overlay =>
+        $sideProperty.map {
+          case Drawer.Side.Start => "0"
+          case Drawer.Side.End   => "auto"
+        }
+    }
+
+  private val panelShellRightProperty =
+    $modeProperty.flatMap {
+      case Drawer.Mode.Push =>
+        $sideProperty.map(_ => "auto")
+
+      case Drawer.Mode.Overlay =>
+        $sideProperty.map {
+          case Drawer.Side.Start => "auto"
+          case Drawer.Side.End   => "0"
+        }
+    }
+
+  private val panelShellTransformProperty =
+    $modeProperty.flatMap {
+      case Drawer.Mode.Push =>
+        $openProperty.map(_ => "translateX(0)")
+
+      case Drawer.Mode.Overlay =>
+        $openProperty.flatMap { open =>
+          $sideProperty.map {
+            case Drawer.Side.Start =>
+              if (open) "translateX(0)" else "translateX(-100%)"
+
+            case Drawer.Side.End =>
+              if (open) "translateX(0)" else "translateX(100%)"
+          }
+        }
+    }
+
+  private val panelShellPointerEventsProperty =
+    $modeProperty.flatMap {
+      case Drawer.Mode.Push =>
+        $openProperty.map(_ => "auto")
+
+      case Drawer.Mode.Overlay =>
+        $openProperty.map { open =>
+          if (open) "auto" else "none"
+        }
+    }
+
+  private val scrimOpacityProperty =
+    $modeProperty.flatMap {
+      case Drawer.Mode.Push =>
+        $openProperty.map(_ => "0")
+
+      case Drawer.Mode.Overlay =>
+        $openProperty.map { open =>
+          if (open) "1" else "0"
+        }
+    }
+
+  private val scrimPointerEventsProperty =
+    $modeProperty.flatMap {
+      case Drawer.Mode.Push =>
+        $openProperty.map(_ => "none")
+
+      case Drawer.Mode.Overlay =>
+        $openProperty.map { open =>
+          if (open) "auto" else "none"
+        }
     }
 
   override def compose(): Unit = {
     given Component = this
+
     addBaseClass("jfx-drawer")
     classIf("jfx-drawer--open", $openProperty)
     classIf("jfx-drawer--start", $sideProperty.map(_ == Drawer.Side.Start))
     classIf("jfx-drawer--end", $sideProperty.map(_ == Drawer.Side.End))
-    
-    style { 
+    classIf("jfx-drawer--push", $modeProperty.map(_ == Drawer.Mode.Push))
+    classIf("jfx-drawer--overlay", $modeProperty.map(_ == Drawer.Mode.Overlay))
+
+    style {
       display = "flex"
+      flexDirection =(drawerFlexDirectionProperty)
       width = "100%"
       height = "100%"
       position = "relative"
+      overflow = "hidden"
     }
 
     Box.box("div") {
       addClass("jfx-drawer__panel-shell")
-      style { 
+
+      style {
         height = "100%"
-        position = "relative"
-        width_=(panelShellWidthProperty)
+        position =(panelShellPositionProperty)
+        top = "0"
+        bottom = "0"
+        left =(panelShellLeftProperty)
+        right =(panelShellRightProperty)
+        width =(panelShellWidthProperty)
+        overflow = "hidden"
+        transform =(panelShellTransformProperty)
+        pointerEvents =(panelShellPointerEventsProperty)
+        transition = "width 180ms ease, transform 180ms ease"
+        zIndex = "20"
       }
 
       Box.box("div") {
         addClass("jfx-drawer__panel")
-        style { 
+
+        style {
           height = "100%"
           overflow = "hidden"
-          width_=($drawerWidthProperty)
+          width_=(resolvedDrawerWidthProperty)
         }
-        
+
         _navigationHost = Box.box("div") {
           addClass("jfx-drawer__navigation")
-          style { 
-            height = "100%" 
+
+          style {
+            height = "100%"
             display = "flex"
             flexDirection = "column"
           }
@@ -69,6 +184,20 @@ class Drawer extends Box("div") {
 
     Box.box("div") {
       addClass("jfx-drawer__scrim")
+
+      style {
+        position = "absolute"
+        top = "0"
+        right = "0"
+        bottom = "0"
+        left = "0"
+        opacity =  scrimOpacityProperty
+        pointerEvents = scrimPointerEventsProperty
+        backgroundColor = "rgba(0, 0, 0, 0.32)"
+        transition = "opacity 180ms ease"
+        zIndex = "10"
+      }
+
       onClick { _ =>
         if ($closeOnScrimClickProperty.get && $openProperty.get) {
           $openProperty.set(false)
@@ -78,12 +207,13 @@ class Drawer extends Box("div") {
 
     _contentHost = Box.box("div") {
       addClass("jfx-drawer__content")
-      style { 
+
+      style {
         height = "100%"
         flex = "1"
         display = "flex"
         flexDirection = "column"
-        minWidth = "0" 
+        minWidth = "0"
       }
     }
 
@@ -104,6 +234,11 @@ class Drawer extends Box("div") {
 object Drawer {
   enum Side { case Start, End }
 
+  enum Mode {
+    case Push
+    case Overlay
+  }
+
   def drawer(init: Drawer ?=> Unit): Drawer = {
     DslRuntime.build(new Drawer())(init)
   }
@@ -123,12 +258,21 @@ object Drawer {
   def open(using d: Drawer): Boolean = d.$openProperty.get
   def open_=(v: Boolean)(using d: Drawer): Unit = d.$openProperty.set(v)
   def openProperty(using d: Drawer): Property[Boolean] = d.$openProperty
-  
+
   def side(using d: Drawer): Side = d.$sideProperty.get
   def side_=(s: Side)(using d: Drawer): Unit = d.$sideProperty.set(s)
+
+  def mode(using d: Drawer): Mode = d.$modeProperty.get
+  def mode_=(m: Mode)(using d: Drawer): Unit = d.$modeProperty.set(m)
+  def modeProperty(using d: Drawer): Property[Mode] = d.$modeProperty
 
   def drawerWidth(using d: Drawer): String = d.$drawerWidthProperty.get
   def drawerWidth_=(w: String)(using d: Drawer): Unit = d.$drawerWidthProperty.set(w)
 
-  def toggle()(using d: Drawer): Unit = d.$openProperty.set(!d.$openProperty.get)
+  def closeOnScrimClick(using d: Drawer): Boolean = d.$closeOnScrimClickProperty.get
+  def closeOnScrimClick_=(v: Boolean)(using d: Drawer): Unit =
+    d.$closeOnScrimClickProperty.set(v)
+
+  def toggle()(using d: Drawer): Unit =
+    d.$openProperty.set(!d.$openProperty.get)
 }
